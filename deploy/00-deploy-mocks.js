097eb661c83b0027249d4d1bc4c535a04253ae9e
Alex <aslaterm32@gmail.com>
2023-02-16 17:38:59 +0000

    modified:   deploy/00-deploy-mocks.js

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -1,5 +1,9 @@
 const { network } = require("hardhat")
-const { developmentChains, DECIMALS, INITIAL_ANSWER } = require("../helper-hardhat-config")
+const {
+    developmentChains,
+    DECIMALS,
+    INITIAL_ANSWER
+} = require("../helper-hardhat-config")
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
@@ -11,12 +15,11 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
             contract: "MockV3Aggregator",
             from: deployer,
             log: true,
-            args: [DECIMALS, INITIAL_ANSWER],
+            args: [DECIMALS, INITIAL_ANSWER]
         })
         log("Mocks deployed")
         log("-----------------------------------------------------")
     }
-
 }
 
 module.exports.tags = ["all", "mocks"]
